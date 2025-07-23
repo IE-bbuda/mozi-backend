9556ef12b819fc5061c6f62b47c5d68397c666f0
SooHyeon Lee <dalsu4594@naver.com>
2025-07-23 17:02:07 +0900

    feat: API 예외 처리 클래스 ApiExceptionAdvice 추가

---
+++ src/main/java/org/iebbuda/mozi/exception/ApiExceptionAdvice.java
@@ -0,0 +1,10 @@
+package org.iebbuda.mozi.exception;
+
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Order(2)
+public class ApiExceptionAdvice {
+}
