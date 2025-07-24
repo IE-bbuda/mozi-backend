cf165665d26844fe4393f0b0e72b701a6025af5c
SooHyeon Lee <dalsu4594@naver.com>
2025-07-25 03:27:40 +0900

    feat: UserMapper에 아이디 찾기 쿼리 추가(이메일 전화번호)

--- src/main/java/org/iebbuda/mozi/user/mapper/UserMapper.java
+++ src/main/java/org/iebbuda/mozi/user/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package org.iebbuda.mozi.user.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.iebbuda.mozi.security.account.domain.AuthVO;
 import org.iebbuda.mozi.user.domain.UserVO;
 
@@ -8,4 +9,6 @@ public interface UserMapper {
     UserVO findByLoginId(String loginId);
     int insert(UserVO userVO);
     int insertAuth(AuthVO auth);
+    String findLoginIdByEmail(@Param("username")String username, @Param("email")String email);
+    String findLoginIdByPhoneNumber(@Param("username")String username, @Param("phoneNumber")String phoneNumber);
 }
