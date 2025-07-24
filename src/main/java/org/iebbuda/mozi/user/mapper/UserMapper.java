0b55fc6e825c41cd5baf84bb580c995c5114932c
SooHyeon Lee <dalsu4594@naver.com>
2025-07-24 10:46:08 +0900

    feat: UserMapper 인터페이스 및 MyBatis XNL 매핑 추가

--- src/main/java/org/iebbuda/mozi/user/mapper/UserMapper.java
+++ src/main/java/org/iebbuda/mozi/user/mapper/UserMapper.java
@@ -1,4 +1,11 @@
 package org.iebbuda.mozi.user.mapper;
 
+import org.iebbuda.mozi.security.account.domain.AuthVO;
+import org.iebbuda.mozi.user.domain.UserVO;
+
 public interface UserMapper {
+    UserVO findByUserId(int userId);
+    UserVO findByLoginId(String loginId);
+    int insert(UserVO userVO);
+    int insertAuth(AuthVO auth);
 }
