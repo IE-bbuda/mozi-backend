8bc4dad040621c213ba68c2f5d5e4a97b9100ba3
SooHyeon Lee <dalsu4594@naver.com>
2025-07-25 03:37:43 +0900

    feat: 아이디 찾기 REST API 구현

--- src/main/java/org/iebbuda/mozi/user/controller/UserController.java
+++ src/main/java/org/iebbuda/mozi/user/controller/UserController.java
@@ -3,6 +3,8 @@ package org.iebbuda.mozi.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.iebbuda.mozi.user.dto.LoginIdFindByEmailRequestDTO;
+import org.iebbuda.mozi.user.dto.LoginIdFindResponseDTO;
 import org.iebbuda.mozi.user.dto.UserJoinRequestDTO;
 import org.iebbuda.mozi.user.dto.UserJoinResponseDTO;
 import org.iebbuda.mozi.user.service.UserService;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -25,5 +27,17 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<UserJoinResponseDTO> signUpUser(@RequestBody UserJoinRequestDTO userJoinRequestDTO){
         return ResponseEntity.ok().body(userService.join(userJoinRequestDTO));
+    }
+
+    @PostMapping("/find-id/email")
+    public ResponseEntity<LoginIdFindResponseDTO> findLoginIdByEmail(
+            @RequestBody LoginIdFindByEmailRequestDTO request) {
+
+        LoginIdFindResponseDTO response = userService.findLoginIdByEmail(
+                request.getUsername(),
+                request.getEmail()
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
