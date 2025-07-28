57f57242bbaf7b75f7fe67717fff27a59dcb8b42
DESKTOP-I1GS1V5\jangs <jangsuyeong923@naver.com>
2025-07-29 00:26:12 +0900

    feat: 적금 상품 DTO/응답 서비스 구현

---
+++ src/main/java/org/iebbuda/mozi/domain/product/controller/SavingController.java
@@ -0,0 +1,31 @@
+package org.iebbuda.mozi.domain.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.iebbuda.mozi.domain.product.dto.SavingResponse;
+import org.iebbuda.mozi.domain.product.service.SavingQueryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/savings")
+@RequiredArgsConstructor
+public class SavingController {
+
+    private final SavingQueryService savingQueryService;
+
+    //전체 적금 목록 조회
+    @GetMapping
+    public List<SavingResponse> getAllSavingProduct(){
+        return savingQueryService.getAllSavings();
+    }
+
+    //특정 적금 상세조회
+    @GetMapping("/{id}")
+    public SavingResponse getSavingProductById(@PathVariable Long id){
+        return savingQueryService.getSavingById(id);
+    }
+}
