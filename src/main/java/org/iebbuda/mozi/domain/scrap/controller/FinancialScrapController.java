d05f89e8be2195763021c50f3542901bce84aae7
DESKTOP-I1GS1V5\jangs <jangsuyeong923@naver.com>
2025-08-07 11:02:04 +0900

    feat: 유저 로그인 정보 기반 스크랩 기능 연동

--- src/main/java/org/iebbuda/mozi/domain/scrap/controller/FinancialScrapController.java
+++ src/main/java/org/iebbuda/mozi/domain/scrap/controller/FinancialScrapController.java
@@ -8,6 +8,8 @@ import org.iebbuda.mozi.domain.scrap.dto.FinancialScrapDto;
 import org.iebbuda.mozi.domain.scrap.service.DepositScrapService;
 import org.iebbuda.mozi.domain.scrap.service.FinancialScrapService;
 import org.iebbuda.mozi.domain.scrap.service.SavingScrapService;
+import org.iebbuda.mozi.domain.security.account.domain.CustomUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,23 +23,27 @@ public class FinancialScrapController {
 
     // 스크랩 목록 조회
     @GetMapping
-    public List<FinancialScrapDto> getUserScraps(@RequestParam Long userId) {
+    public List<FinancialScrapDto> getUserScraps(@AuthenticationPrincipal CustomUser user) {
+        long userId= (long) user.getUser().getUserId();
+        System.out.println("userId체크: "+userId);
         return financialScrapService.getUserScraps(userId);
     }
 
     // 스크랩 추가
     @PostMapping
-    public void addScrap(@RequestParam Long userId,
+    public void addScrap(@AuthenticationPrincipal CustomUser user,
                          @RequestParam String productType,
                          @RequestParam Long productId) {
+        long userId= (long) user.getUser().getUserId();
         financialScrapService.addScrap(userId, productType, productId);
     }
 
     // 스크랩 삭제
     @DeleteMapping
-    public void removeScrap(@RequestParam Long userId,
+    public void removeScrap(@AuthenticationPrincipal CustomUser user,
                             @RequestParam String productType,
                             @RequestParam Long productId) {
+        long userId= (long) user.getUser().getUserId();
         financialScrapService.removeScrap(userId, productType, productId);
     }
 }
